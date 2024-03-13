f8e444ff10a832121a760e8b9a04f36b6d3be806
[Sumanta Sahoo] <[sumantasahoo138@gmail.com]>
2024-03-13 17:55:28 +0530

    Backend SpringBoot Initialiser Folder Setting Up

---
+++ Backend/com.VechileCare/com.VechileCare/src/main/java/com/VechileCare/com/VechileCare/VechileCareApplication.java
@@ -0,0 +1,13 @@
+package com.VechileCare.com.VechileCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VechileCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VechileCareApplication.class, args);
+	}
+
+}
---
+++ Backend/com.VechileCare/com.VechileCare/src/test/java/com/VechileCare/com/VechileCare/VechileCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VechileCare.com.VechileCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VechileCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
